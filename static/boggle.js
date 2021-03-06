405d2d66fa83f9ae8b4bcca3c3549c10f15feb47
Kathrin Neyzberg <kneyzberg@gmail.com>
2021-03-05 17:18:53 -0800

    update front end of boggle

--- static/boggle.js
+++ static/boggle.js
@@ -14,6 +14,7 @@ let gameId;
 async function start() {
   let response = await axios.get("/api/new-game");
   gameId = response.data.gameId;
+  console.log(response.data);
   let board = response.data.board;
 
   displayBoard(board);
@@ -22,9 +23,36 @@ async function start() {
 /** Display board */
 
 function displayBoard(board) {
-  // $table.empty();
+  // console.log("hello");
+  $table.empty();
   // loop over board and create the DOM tr/td structure
+  $table.append(`<tbody id="table-body">`)
+  for (let i = 0; i < board.length; i++) {
+    $("#table-body").append(`<tr id=${i}>`);
+    for (let j = 0; j < board[i].length; j++) {
+      $(`#${i}`).append(`<td>${board[i][j]}</td>`);
+    }
+  }
 }
 
-
+function displayResult(word, wordResult) {
+  if (wordResult.result === "ok") {
+    $playedWords.append(`<li>${word}</li>`);
+    $message.text("");
+  } else {
+    $message.text(`This is ${wordResult.result}`);
+  }
+}
 start();
+$form.on("submit", async function (e) {
+  e.preventDefault();
+  let word = $wordInput.val();
+  let wordResult = await axios({
+    method: "post",
+    url: "/api/score-word",
+    data: { gameId, word }
+  });
+  displayResult(word, wordResult.data);
+});
+
+
